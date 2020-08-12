3cc1f31bcf9d8d49abcb1c3d59d3e3afa1536e7e
Michael Duong <michael@turntlabs.com>
2020-08-12 01:00:51 -0700

    Initial Commit

---
+++ square-mobile-projectTests/square_mobile_projectTests.swift
@@ -0,0 +1,33 @@
+//
+//  square_mobile_projectTests.swift
+//  square-mobile-projectTests
+//
+//  Created by Swift Team Six on 8/12/20.
+//
+
+import XCTest
+@testable import square_mobile_project
+
+class square_mobile_projectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
